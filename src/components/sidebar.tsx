dc778a98e9e2ccdb6383e6fdd6142bb420f11c13
Makhare01 <maxare2015@gmail.com>
2025-06-12 12:05:13 +0400

    fix: remove recent tokens dummy data and update email

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,38 +1,14 @@
 import Link from 'next/link';
 
-const recentTokens = [
-  {
-    name: 'TRU',
-    website: 'TrueFi',
-  },
-  {
-    name: 'ETH',
-    website: 'Ethereum',
-  },
-  {
-    name: 'LINK',
-    website: 'Chainlink',
-  },
-];
-
 export const Sidebar = () => {
   return (
     <div className="h-full min-w-[300px] border-r bg-white p-6">
       <Link className="text-2xl font-bold text-text-main md:hidden" href="/dashboard">
         Compliance
       </Link>
-      <h3 className="text-sm font-medium text-gray-500 mb-2 md:mt-0 mt-8">Recent Tokens</h3>
+      <h3 className="text-sm font-bold text-gray-500 mb-2 md:mt-0 mt-8">Recent Tokens</h3>
 
-      <ul className="space-y-1">
-        {recentTokens.map((token) => (
-          <li
-            key={token.name + token.website}
-            className="text-sm hover:text-hawk-primary cursor-pointer"
-          >
-            {token.name} - {token.website}
-          </li>
-        ))}
-      </ul>
+      <p className="text-sm text-gray-500 mt-3">Coming soon</p>
     </div>
   );
 };
